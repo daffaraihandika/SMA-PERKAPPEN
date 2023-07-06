bd0dd6b9b6865020659bc2285019c0dd79e6415b
daffa <draihandika@gmail.com>
2023-07-07 00:03:58 +0700

    buat tampilan informasi sekolah

--- main.js
+++ main.js
@@ -13,3 +13,19 @@ function openTab(tabname){
     event.currentTarget.classList.add("link-active");
     document.getElementById(tabname).classList.add("content-active");
 }
+
+var btnSidebars = document.getElementsByClassName("btn-sidebar")
+var contents = document.getElementsByClassName("content")
+
+function openTabs(tabname){
+    for(tablink of btnSidebars){
+        tablink.classList.remove("btn-active");
+    }
+
+    for(tabcontent of contents){
+        tabcontent.classList.remove("content-informasi-active");
+    }
+
+    event.currentTarget.classList.add("btn-active");
+    document.getElementById(tabname).classList.add("content-informasi-active");
+}
